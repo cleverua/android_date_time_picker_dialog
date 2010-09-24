8f6a94a4c7608a043be620e9f633bc9c4eda37b6
Vitaliy Khudenko <vitaliy.khudenko@gmail.com>
2010-09-24 11:05:01 +0300

    minor code formatting fix

--- src/com/cleverua/android/BaseActivity.java
+++ src/com/cleverua/android/BaseActivity.java
@@ -6,72 +6,72 @@ import android.util.Log;
 
 public class BaseActivity extends Activity {
 
-	private String tag;
+    private String tag;
     protected boolean isCleanStart; // whether this is a clean start or a restore
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		logInfo("onCreate");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        logInfo("onCreate");
         isCleanStart = (savedInstanceState == null);
         logInfo("onCreate: isCleanStart = " + isCleanStart);
-	}
-	
+    }
+
     @Override
     protected void onStart() {
-    	super.onStart();
-    	logInfo("onStart");
+        super.onStart();
+        logInfo("onStart");
     }
-    
+
     @Override
     protected void onResume() {
-    	super.onResume();
-    	logInfo("onResume");
+        super.onResume();
+        logInfo("onResume");
     }
-    
+
     @Override
     protected void onRestart() {
-    	super.onRestart();
-    	logInfo("onRestart");
+        super.onRestart();
+        logInfo("onRestart");
     }
-    
+
     @Override
     protected void onPause() {
-    	super.onPause();
-    	logInfo("onPause");
+        super.onPause();
+        logInfo("onPause");
     }
-    
+
     @Override
     protected void onStop() {
-    	super.onStop();
-    	logInfo("onStop");
+        super.onStop();
+        logInfo("onStop");
     }
-    
+
     @Override
     protected void onDestroy() {
-    	super.onDestroy();
-    	logInfo("onDestroy");
+        super.onDestroy();
+        logInfo("onDestroy");
     }
-    
+
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
-    	super.onRestoreInstanceState(savedInstanceState);
-    	logInfo("onRestoreInstanceState");
+        super.onRestoreInstanceState(savedInstanceState);
+        logInfo("onRestoreInstanceState");
     }
-    
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-    	super.onSaveInstanceState(outState);
-    	logInfo("onSaveInstanceState");
+        super.onSaveInstanceState(outState);
+        logInfo("onSaveInstanceState");
     }
-    
-	protected String getTag() {
-		if (tag == null) {
-			tag = this.getString(R.string.log_tag_prefix) + ' ' + this.getClass().getSimpleName();
-		}
-		return tag;
-	}
-	
+
+    protected String getTag() {
+        if (tag == null) {
+            tag = getString(R.string.log_tag_prefix) + ' ' + getClass().getSimpleName();
+        }
+        return tag;
+    }
+
     protected void log(String msg) {
         Log.d(getTag(), msg);
     }
@@ -79,7 +79,7 @@ public class BaseActivity extends Activity {
     protected void logInfo(String msg) {
         Log.i(getTag(), msg);
     }
-    
+
     protected void log(String msg, Throwable tr) {
         Log.e(getTag(), msg, tr);
     }
--- src/com/cleverua/android/DateHelper.java
+++ src/com/cleverua/android/DateHelper.java
@@ -7,95 +7,95 @@ import android.widget.DatePicker;
 import android.widget.TimePicker;
 
 public class DateHelper {
-    
-	private static final SimpleDateFormat FULL_DATE_FORMATTER = new SimpleDateFormat(
-		"MM/dd/yyyy 'at' HH:mm"
-	);
-	
-	private static final SimpleDateFormat SHORT_DATE_FORMATTER = new SimpleDateFormat(
+
+    private static final SimpleDateFormat FULL_DATE_FORMATTER = new SimpleDateFormat(
+        "MM/dd/yyyy 'at' HH:mm"
+    );
+
+    private static final SimpleDateFormat SHORT_DATE_FORMATTER = new SimpleDateFormat(
         "MM/dd/yyyy"
-	);
-	
-	public static String format(long date) {
-		return FULL_DATE_FORMATTER.format(date);
-	}
-	
+    );
+
+    public static String format(long date) {
+        return FULL_DATE_FORMATTER.format(date);
+    }
+
     public static String formatShortly(long date) {
-		return SHORT_DATE_FORMATTER.format(date);
-	}
-	
-	public static int getDayOfMonth(long date) {
-		Calendar c = Calendar.getInstance();
-		c.setTimeInMillis(date);
-		return c.get(Calendar.DAY_OF_MONTH);
-	}
-	
-	public static int getMonthOfYear(long date) {
-		Calendar c = Calendar.getInstance();
-		c.setTimeInMillis(date);
-		return c.get(Calendar.MONTH);
-	}
-	
-	public static int getYear(long date) {
-		Calendar c = Calendar.getInstance();
-		c.setTimeInMillis(date);
-		return c.get(Calendar.YEAR);
-	}
-	
+        return SHORT_DATE_FORMATTER.format(date);
+    }
+
+    public static int getDayOfMonth(long date) {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(date);
+        return c.get(Calendar.DAY_OF_MONTH);
+    }
+
+    public static int getMonthOfYear(long date) {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(date);
+        return c.get(Calendar.MONTH);
+    }
+
+    public static int getYear(long date) {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(date);
+        return c.get(Calendar.YEAR);
+    }
+
     public static int getHourOfDay(long date) {
         Calendar c = Calendar.getInstance();
         c.setTimeInMillis(date);
         return c.get(Calendar.HOUR_OF_DAY);
     }
-    
+
     public static int getMinute(long date) {
         Calendar c = Calendar.getInstance();
         c.setTimeInMillis(date);
         return c.get(Calendar.MINUTE);
     }
-	
-	public static long getDate(int year, int monthOfYear, int dayOfMonth) {
-		Calendar c = Calendar.getInstance();
-		c.set(year, monthOfYear, dayOfMonth);
-		return c.getTimeInMillis();
-	}
-	
-	/**
-	 * @param datePicker
-	 * @return date with the hours/minutes/seconds/milliseconds stripped off
-	 */
-	public static long getDate(DatePicker datePicker) {
+
+    public static long getDate(int year, int monthOfYear, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(year, monthOfYear, dayOfMonth);
+        return c.getTimeInMillis();
+    }
+
+    /**
+     * @param datePicker
+     * @return date with the hours/minutes/seconds/milliseconds stripped off
+     */
+    public static long getDate(DatePicker datePicker) {
         return getDate(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth());
     }
-	
-	public static void initDatePicker(DatePicker datePicker, long date) {
-	    datePicker.init(getYear(date), getMonthOfYear(date), getDayOfMonth(date), null);
+
+    public static void initDatePicker(DatePicker datePicker, long date) {
+        datePicker.init(getYear(date), getMonthOfYear(date), getDayOfMonth(date), null);
     }
-	
+
     public static void initTimePicker(TimePicker timePicker, long date) {
         timePicker.setCurrentMinute(getMinute(date));
         timePicker.setCurrentHour(getHourOfDay(date));
     }
-	
+
     /**
      * @param timePicker
-     * @return time as milliseconds since 00:00:00:000 (midnight) 
+     * @return time as milliseconds since 00:00:00:000 (midnight)
      */
     public static long getTime(TimePicker timePicker) {
         int h = timePicker.getCurrentHour().intValue();
         int m = timePicker.getCurrentMinute().intValue();
         return (h * 3600 + m * 60) * 1000;
     }
-    
-	public static long getStartOfTheDay(long date) {
-	    Calendar c = Calendar.getInstance();
+
+    public static long getStartOfTheDay(long date) {
+        Calendar c = Calendar.getInstance();
         c.setTimeInMillis(date);
-        
+
         c.set(Calendar.HOUR_OF_DAY, 0);
         c.set(Calendar.MINUTE, 0);
         c.set(Calendar.SECOND, 0);
         c.set(Calendar.MILLISECOND, 0);
-        
+
         return c.getTimeInMillis();
-	}
+    }
 }
